a4d8393c358b332bdbf5e95ea1ee30fb5b5ee78e
Anubhav Bagri <anubhav.199302029@muj.manipal.edu>
2022-06-24 04:51:26 +0530

    feat: provider depending on another

--- lib/pages/counter_state_provider_page.dart
+++ lib/pages/counter_state_provider_page.dart
@@ -3,6 +3,20 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 final _counterProvider = StateProvider<int>((ref) => 0);
 
+final _isEvenProvider = Provider<bool>((ref) {
+  final counter = ref.watch(_counterProvider);
+  return (counter % 2 == 0);
+});
+
+final _evenCounter = Provider<int>((ref) {
+  ref.listen<bool>(_isEvenProvider, (value, val) {
+    if (val) {
+      ref.state++;
+    }
+  });
+  return 0;
+});
+
 class CounterStateProviderPage extends ConsumerWidget {
   const CounterStateProviderPage({Key? key}) : super(key: key);
 
@@ -14,7 +28,14 @@ class CounterStateProviderPage extends ConsumerWidget {
         title: Text('State Provider Page'),
       ),
       body: Center(
-        child: Text('Count: ${counter}'),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Count: ${counter}'),
+            CounterIsEven(),
+            EvenCounter(),
+          ],
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
@@ -23,5 +44,26 @@ class CounterStateProviderPage extends ConsumerWidget {
         child: Icon(Icons.add),
       ),
     );
+  }
+}
+
+class CounterIsEven extends ConsumerWidget {
+  const CounterIsEven({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    // print('Building');
+    final isEven = ref.watch(_isEvenProvider);
+    return Text(isEven ? 'Is Even' : 'Not even');
+  }
+}
+
+class EvenCounter extends ConsumerWidget {
+  const EvenCounter({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final evenCount = ref.watch(_evenCounter);
+    return Text('$evenCount');
   }
 }
